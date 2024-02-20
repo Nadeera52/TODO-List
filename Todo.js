43e324c2d04047c4b63e381774a634de39ca7fe3
nadeera <nadeeramuhammed52@gmail.com>
2024-02-20 12:16:06 +0530

    files saved

---
+++ Todo.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import './Todo.css'
+import Listtask from './ListTasks'
+import AddTask from './AddTask'
+const Todo = () => {
+  const [tasks,setTasks] =useState([]);
+
+  const addTask=(title)=>{
+const  newTask = [...tasks,{title}]
+setTasks(newTask)
+  }
+  const removeTask =(index)=>{
+    const newTask = [...tasks]
+    newTask.splice(index,1)
+    setTasks(newTask)
+  }
+  return (
+    <>
+     <div className='todo-container'>
+        <div className='header'>TODO APP</div>
+        <div className='add-tasks'><AddTask addTask={addTask}/>
+        </div>
+        <div className='tasks'>
+          {tasks.map((task,index)=>(
+ <Listtask task={task} removeTask={removeTask}
+ index={index} key={index}
+ />  
+          ))}
+                
+        </div>
+     </div>
+    </>
+  )
+}
+
+export default Todo
